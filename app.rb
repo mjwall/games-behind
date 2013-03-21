fe784d8a2a762a080b3d87e000983936eebfde0b
Michael Wall <mjwall@gmail.com>
2013-03-20 21:07:19 -0400

    Add haml

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require 'sinatra/base'
+require 'haml'
 
 class MyApp < Sinatra::Base
 
   get '/' do
+    haml :index
+  end
+
+  get '/orig' do
     "the time where this server lives is #{Time.now}
     <br /><br />check out your <a href=\"/agent\">user_agent</a>"
   end
