35b34a54ce651169a6a32a45eaefa84ae1c2a8bf
Michael Wall <mjwall@gmail.com>
2013-03-30 22:23:48 -0400

    Rake task to fetch latest
    Logic to check for existing file, check date on file and write if needed.
    
    Still need to add sendmail.  This should be able to be called every hour, at least at first so I can make sure it is working

--- lib/models.rb
+++ lib/models.rb
@@ -1,20 +1,56 @@
 require 'active_support/core_ext/hash'
 require 'json'
+require 'open-uri'
 
 class Daily
-  attr_accessor :xml, :data_dir
+  attr_accessor :xml, :data_dir, :hash
 
   def self.from_xml_file date_str, data_dir
     unless /\d{8}/.match date_str
       raise RuntimeError.new "Not an 8 digit date"
     end
+    xml_file = "#{data_dir}/#{date_str[0..3]}/#{date_str}.xml"
     d = Daily.new
     d.data_dir = data_dir
-    d.xml= File.read("#{data_dir}/#{date_str[0..3]}/#{date_str}.xml")
+    d.xml= File.read(xml_file)
     d
   end
 
+  def hash
+    @hash ||= Hash.from_xml(@xml)
+  end
+
   def json
-    Hash.from_xml(@xml).to_json
+    hash.to_json
+  end
+
+  def file_date
+    # file usually comes out early in the day and only contains data for the previous day's games
+    DateTime.parse hash['sports_content']['sports_metadata']['date_time']
+  end
+
+  def persist
+    raise RuntimeError.new "No data directory specificed" if data_dir == nil
+    # will be stored with previous days date as filename
+    prior_date = (file_date - 1).strftime('%Y%m%d')
+    location = "#{data_dir}/#{prior_date[0..3]}/#{prior_date}.xml"
+    raise RuntimeError.new "Not overwriting, File already exists, #{location}" if File.exists?(location)
+    File.open(location,'w') do |f|
+      f.write @xml
+    end
+    puts "File saved to @{location}"
+  end
+
+  def persist_to data_dir
+    @data_dir = data_dir
+    persist
+  end
+
+  def self.fetch_latest
+    d = Daily.new
+    open("http://erikberg.com/mlb/standings.xml") do |f|
+      d.xml = f.read
+    end
+    d
   end
 end
--- lib/tasks/fetch.rake
+++ lib/tasks/fetch.rake
@@ -2,17 +2,35 @@ require_relative "../models.rb"
 
 desc "Fetch lastest XML"
 task :fetch do
-  puts "Checking latest XML"
+  msg = "Running rake fetch"
   data_dir = File.dirname(__FILE__) + "/../../data"
-  puts data_dir
-  yesterday_str = (Date.today - 1).strftime('%Y%m%d')
-  yesterday_str = "20120416"
+  #yesterday_str = (Date.today - 1).strftime('%Y%m%d')
+  # to test file delivery, remove 20121003.xml
+  yesterday_str = "20121003"
+  # to test existing file
+  # yesterday_str = "20121002"
+  msg += "\nYesterday: #{yesterday_str}\nDataDir: #{data_dir}"
   begin
-    daily = Daily.from_xml_file yesterday_str, data_dir
-    puts "File exists for #{yesterday_str}"
-  rescue Errno::ENOENT
-    puts "File missing, let's go get it"
+    msg += "\nChecking if we already have the latest"
+    existing = Daily.from_xml_file yesterday_str, data_dir
+    msg += "\nYep: File exists for #{yesterday_str}"
+  rescue Errno::ENOENT => e
+    # not really an error
+    msg += "\nNope: " + e.message
   end
-  # if file for yesterday doesn't exist, go get it
-  # if it does exist, make sure the date in the xml is today
+  if existing == nil
+    msg += "\nGetting the latest from the internet"
+    daily = Daily.fetch_latest
+    begin
+      daily.persist_to data_dir
+      msg += "\nSaved file-----------\n"
+      msg += daily.xml
+    rescue RuntimeError => e
+      msg += "\nERROR: " + e.message
+      msg += "\nDate on file: #{daily.file_date.strftime('%Y%m%d')}"
+      msg += "\nShould have been today's data"
+    end
+  end
+  # sendmail here
+  puts msg
 end
